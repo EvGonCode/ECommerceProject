334922bdaad8f79c6b6b59bd511547b338e2cd03
Honcharov Yevhen <vip.honcharov@gmail.com>
2025-04-07 01:13:59 +0300

    Initial auth service
    Base functionality

---
+++ authentication-service/src/main/java/com/authentication_service/domain/User.java
@@ -0,0 +1,58 @@
+package com.authentication_service.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Data
+@Builder
+@Entity
+@AllArgsConstructor
+@Table(name="User")
+public class User{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Integer id;
+
+    @Column(unique = true)
+    private String login;
+    private String password;
+
+    @Builder.Default
+    @Column(nullable = false)
+    private String role = "USER";
+
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(int customerId) {
+        this.id = customerId;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
