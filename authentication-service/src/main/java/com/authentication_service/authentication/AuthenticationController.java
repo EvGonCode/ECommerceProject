07fa9bd2bfb33b41a99b309a78d81dee318a0ce2
Honcharov Yevhen <vip.honcharov@gmail.com>
2025-04-20 01:08:29 +0200

    jwt issue fixed

--- authentication-service/src/main/java/com/authentication_service/authentication/AuthenticationController.java
+++ authentication-service/src/main/java/com/authentication_service/authentication/AuthenticationController.java
@@ -1,17 +1,20 @@
 package com.authentication_service.authentication;
 
 
+import com.authentication_service.config.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import com.authentication_service.config.JwtService;
 
 @RestController
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final AuthenticationService service;
+    private final JwtService jwtService;
 
     @PostMapping("/registerUser")
     public ResponseEntity<AuthenticationResponse> registerUser(@RequestBody RegisterRequest request){
@@ -31,7 +34,7 @@ public class AuthenticationController {
 
     @PostMapping("/verify_jwt")
     public ResponseEntity<String> verifyJwt(@RequestBody String jwt) {
-        if(service.isTokenValid(jwt)) return ResponseEntity.ok("Token is valid");
+        if(service.isTokenValid(jwt)) return ResponseEntity.ok(JwtService.extractLogin(jwt));
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Token is not valid");
     }
 }
